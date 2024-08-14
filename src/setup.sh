481b7842761dfd2ecac8294067872c5e6314df7f
Tara E. Walker <taraew@live.com>
2024-08-14 19:00:10 -0400

    Update to requirements.txt for install libs and WIP: build scripts for SAM2 & CUDA-TEW

---
+++ src/setup.sh
@@ -0,0 +1,12 @@
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.5.1/local_installers/cuda-repo-ubuntu2204-12-5-local_12.5.1-555.42.06-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-5-local_12.5.1-555.42.06-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-5-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-5
+
+pip install -r requirements.txt
+
+git clone https://github.com/facebookresearch/segment-anything-2.git
+cd segment-anything-2 & pip install -e .
